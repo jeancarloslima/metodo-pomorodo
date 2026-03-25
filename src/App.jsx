7e3bd951d0838d27d9c0dbef1159bcbca605d95f
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-25 13:14:59 -0300

    Definindo estrutura inicial e componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import ContadorRegressivo from './components/ContadorRegressivo'
+import EditorTempos from './components/EditorTempos'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='app'>
+      <header>
+        <div className="header-container">
+          <EditorTempos />
+        </div>
+      </header>
+
+      <main>
+        <div className="main-container">
+          <ContadorRegressivo />
+          <EditorTempos />
+        </div>
+      </main>
+    </div>
   )
 }
 
