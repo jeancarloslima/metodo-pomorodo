f304618780e38aa73c21945923a32b36b34ee81a
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-30 13:10:38 -0300

    Adicionando função de alterar tempos de foco e descanso e salvando no LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import './App.css'
-import ContadorRegressivo from './components/ContadorRegressivo'
-import EditorTempos from './components/EditorTempos'
-import ListaTarefas from './components/ListaTarefas'
-import { useState } from 'react'
-import { GiTomato } from 'react-icons/gi'
-import { FaPen } from 'react-icons/fa'
-import { IoSettingsSharp } from 'react-icons/io5'
+import "./App.css";
+import ContadorRegressivo from "./components/ContadorRegressivo";
+import EditorTempos from "./components/EditorTempos";
+import ListaTarefas from "./components/ListaTarefas";
+import { useState } from "react";
+import { GiTomato } from "react-icons/gi";
+import { FaPen } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
+import { PreferenciasTemposContext } from "./contexts/PreferenciasTempoContext";
 
 function App() {
   const [editando, setEditando] = useState(false);
-  const [tempoFoco, setTempoFoco] = useState(25);
-  const [tempoDescanso, setTempoDescanso] = useState(5);
+  const [tempoFoco, setTempoFoco] = useState(
+    Number(localStorage.getItem("tempo-foco")) || 25,
+  );
+  const [tempoDescanso, setTempoDescanso] = useState(
+    Number(localStorage.getItem("tempo-descanso")) || 5,
+  );
 
   return (
-    <div id='app'>
-      <header>
-        <div id="header-container">
-          <h1 id='titulo'>Pomodor<GiTomato /></h1>
-          <button onClick={() => setEditando(!editando)} className='btn-editar-tempos'><IoSettingsSharp /></button>
-          {editando && <EditorTempos />}
-        </div>
-      </header>
+    <PreferenciasTemposContext.Provider value={{ tempoFoco, tempoDescanso, setTempoFoco, setTempoDescanso }}>
+      <div id="app">
+        <header>
+          <div id="header-container">
+            <h1 id="titulo">
+              Pomodor
+              <GiTomato />
+            </h1>
+            <button
+              onClick={() => setEditando(!editando)}
+              className="btn-editar-tempos"
+            >
+              <IoSettingsSharp />
+            </button>
+            {editando && <EditorTempos />}
+          </div>
+        </header>
 
-      <main>
-        <div id="main-container">
-          <ContadorRegressivo />
-          <ListaTarefas />
-        </div>
-      </main>
-    </div>
-  )
+        <main>
+          <div id="main-container">
+            <ContadorRegressivo />
+            <ListaTarefas />
+          </div>
+        </main>
+      </div>
+    </PreferenciasTemposContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/components/EditorTempos/index.jsx
+++ src/components/EditorTempos/index.jsx
@@ -1,18 +1,72 @@
+import { useContext } from "react";
+import { PreferenciasTemposContext } from "../../contexts/PreferenciasTempoContext";
 import "./editor-tempos.css";
 
 export default function EditorTempos() {
-    return (
-        <div className="editor-tempos-container">
-            <div className="container-foco">
-                <button>-</button>
-                <h3 className="tempo-foco">25</h3>
-                <button>+</button>
-            </div>
-            <div className="container-descanso">
-                <button>-</button>
-                <h3 className="tempo-foco">25</h3>
-                <button>+</button>
-            </div>
-        </div>
-    )
+  const { tempoFoco, tempoDescanso, setTempoFoco, setTempoDescanso } =
+    useContext(PreferenciasTemposContext);
+
+  return (
+    <div className="editor-tempos-container">
+      <div className="container-foco">
+        <button
+          onClick={() => {
+            if (tempoFoco === 1) return;
+
+            const novoTempo = tempoFoco - 1;
+
+            setTempoFoco(novoTempo);
+            localStorage.setItem("tempo-foco", novoTempo);
+          }}
+          className="btn-controle btn-foco"
+        >
+          -
+        </button>
+        <h3 className="tempo-foco">{tempoFoco}</h3>
+        <button
+          onClick={() => {
+            if (tempoFoco === 59) return;
+
+            const novoTempo = tempoFoco + 1;
+
+            setTempoFoco(novoTempo);
+            localStorage.setItem("tempo-foco", novoTempo);
+          }}
+          className="btn-controle btn-foco"
+        >
+          +
+        </button>
+      </div>
+
+      <div className="container-descanso">
+        <button
+          onClick={() => {
+            if (tempoDescanso === 1) return;
+
+            const novoTempo = tempoDescanso - 1;
+
+            setTempoDescanso(novoTempo);
+            localStorage.setItem("tempo-descanso", novoTempo);
+          }}
+          className="btn-controle btn-descanso"
+        >
+          -
+        </button>
+        <h3 className="tempo-descanso">{tempoDescanso}</h3>
+        <button
+          onClick={() => {
+            if (tempoDescanso === 59) return;
+
+            const novoTempo = tempoDescanso + 1;
+
+            setTempoDescanso(novoTempo);
+            localStorage.setItem("tempo-descanso", novoTempo);
+          }}
+          className="btn-controle btn-descanso"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
