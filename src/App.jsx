d87a4d4c303af4d76db08893c4846ca77c80afd4
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-30 12:44:07 -0300

    Adicionando configurações ao header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { GiTomato } from 'react-icons/gi'
 import './App.css'
 import ContadorRegressivo from './components/ContadorRegressivo'
 import EditorTempos from './components/EditorTempos'
 import ListaTarefas from './components/ListaTarefas'
+import { useState } from 'react'
+import { GiTomato } from 'react-icons/gi'
+import { FaPen } from 'react-icons/fa'
+import { IoSettingsSharp } from 'react-icons/io5'
 
 function App() {
+  const [editando, setEditando] = useState(false);
+  const [tempoFoco, setTempoFoco] = useState(25);
+  const [tempoDescanso, setTempoDescanso] = useState(5);
 
   return (
     <div id='app'>
       <header>
         <div id="header-container">
           <h1 id='titulo'>Pomodor<GiTomato /></h1>
-          <EditorTempos />
+          <button onClick={() => setEditando(!editando)} className='btn-editar-tempos'><IoSettingsSharp /></button>
+          {editando && <EditorTempos />}
         </div>
       </header>
 
--- src/components/EditorTempos/index.jsx
+++ src/components/EditorTempos/index.jsx
@@ -1,5 +1,18 @@
 import "./editor-tempos.css";
 
 export default function EditorTempos() {
-    
+    return (
+        <div className="editor-tempos-container">
+            <div className="container-foco">
+                <button>-</button>
+                <h3 className="tempo-foco">25</h3>
+                <button>+</button>
+            </div>
+            <div className="container-descanso">
+                <button>-</button>
+                <h3 className="tempo-foco">25</h3>
+                <button>+</button>
+            </div>
+        </div>
+    )
 }
